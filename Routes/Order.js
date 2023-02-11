8c07270871bb10d8aa7eed9b0690d7b20dd5f2bd
sandhya076 <lavurisandhya76@gmail.com>
2023-02-11 22:27:11 +0530

    intialize

---
+++ Routes/Order.js
@@ -0,0 +1,66 @@
+import express from "express";
+import Order from "../model/Order.js";
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const newOrder = new Order(req.body);
+
+  try {
+    const savedOrder = await newOrder.save();
+    res.status(200).json(savedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//UPDATE
+router.put("/:id",  async (req, res) => {
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE
+router.delete("/:id",  async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.status(200).json("Order has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET USER ORDERS
+router.get("/find/:userId",  async (req, res) => {
+  try {
+    const orders = await Order.find({ userId: req.params.userId });
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// //GET ALL
+
+router.get("/",  async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+
+export default router;
